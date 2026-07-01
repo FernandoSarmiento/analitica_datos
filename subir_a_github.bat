39ee8856cc429b222cbe27509f53fe6594f1ea51
¨FernandoSarmiento¨ <fsarmientoma@unjbg.edu.pe>
2026-07-01 13:16:21 -0500

    Actualizacion automatica

--- subir_a_github.bat
+++ subir_a_github.bat
@@ -2,6 +2,6 @@
 REM Script para subir automáticamente los cambios a GitHub
 cd /d "%~dp0"
 git add .
-git commit -m "Actualización automática"
+git commit -m "Actualizacion automatica"
 git push origin master
 pause
